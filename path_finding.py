3c5ad2b4c9a4e134b84551bfb61363f427807eb0
Gennari Lorenzo <lrnzgnnr@gmail.com>
2025-03-06 16:03:52 +0100

    aggiunta getSuccessors e isGoal in path_finding

--- path_finding.py
+++ path_finding.py
@@ -14,7 +14,22 @@ class PathFinding(SearchProblem):
         super().__init__(init, goal, [(a, 1) for a in self.actions])
 
     def getSuccessors(self, state) -> set:
-        raise NotImplementedError("To be implemented")
+        succ = set()
+        for a in self.actions:
+            if a == 'S':
+                next = (state[0], state[1]-1)
+            elif a == 'N':
+                next = (state[0], state[1]+1)
+            elif a == 'W':
+                next = (state[0]-1, state[1])
+            elif a == 'E':
+                next = (state[0]+1, state[1])
+            if next not in self.world.walls and self.isInTheLimits(next):
+                succ.add((a, next))
+        return succ
+
+    def isInTheLimits(self, state):
+        return state[0] >= 0 and state[0] <= self.world.x_lim and state[1] >= 0 and state[1] <= self.world.y_lim
 
     def isGoal(self, state):
-        raise NotImplementedError("To be implemented")
+        return state[0] == self.goal[0] and state[1] == self.goal[1]
